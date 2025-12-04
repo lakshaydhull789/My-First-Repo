9e9ede1d069e62c565ec0e8edc2854e25691d367
lakshaydhull789 <lakshaydhull789@gmail.com>
2025-12-04 22:43:53 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,63 +1,75 @@
-#.....FILE HANDLING
-'''
+#.....csv files
 
-FILE IS A COLLECTION OF DATA
+#...csv
+#....reader(), writer(), writerows()
 
-file is of 3 types...
-1) text file
-2) binary file
-3) CSV file
-
-file operations
-
-1) create
-2) delete
-
-3) rename
-4) open
-5) close
-
-6) read
-7) write
-
-file opening modes
-1) r(read)
-2) w(write)
-3) a(append)
-'''
-
-#...WAP to create a text file "data.txt" and store student names in it
-'''
-a=open("data.txt","a")
-name=input("\n\tEnter Student Name: ")
-a.write(name)
-a.close()
-'''
-
+#...WAP to open and read data from a CSV FILE
 
 '''
-a=open("records","a")
+f=open("bharatnewcsv.csv")
 
-size=int(input("\n\tTotal Names: "))
-for i in range(1,size+1):
+import csv
+
+data=csv.reader(f)
+
+new=[]
+for i in data:
+    print(i)
+    if i[1].upper()=='DELHI':
+        new.append(i)
+
+import tabulate as t
+print(t.tabulate(new,headers=('name','address','salary'),tablefmt='grid'))
+
+
+
+for i in new:
+    if i[1].upper()=='DELHI':
+        print(t.tabulate([i],headers=('name','address','salary'),tablefmt='grid'))
+
+
+for i in new:
+    if i[2]>'10000':
+        print(t.tabulate([i],headers=('name','address','salary'),tablefmt='grid'))
+
+
+'''
+
+
+
+
+
+
+'''
+data=[]
+f=open("bharatnewcsv.csv","w",newline="")
+
+x=int(input("\n\tEnter no of employees: "))
+for i in range(1,x+1):
+    print(f"--------Enter details for Employee Number {i}----------\n")
     name=input("\n\tEnter Name: ")
-    name="Name: "+name+"\n"
-    a.write(name)
-a.close()
-print("\n\t!!!Data Saved!!!")
+    add=input("\n\tEnter address: ")
+    sal=int(input("\n\tEnter Salary: "))
+    data.append([name,add,sal])
+
+
+import csv
+d=csv.writer(f)
+d.writerows(data)
+f.close()
+
+print("\n\tdata saved!!!")
+
 '''
 
-new=open("Empdata.txt.","a")
-l=int(input("\t\tHow many employees data you want to enter"))
-for i in range (0,l):
-    Name=input("\t\tPlease enter employee Name: ")
-    EmpID=input("\t\tPlease enter employee ID:")
-    Des=input("\t\tPlease enter employee designation: ")
-    empdata="\nName: "+Name+"\tEmployee ID: "+EmpID+"\tDesignation: "+Des+"\n"
-    new.write(empdata)
-    new.write("_"*70)
+'''
+with open("sample.txt") as d:
+    print(d.read())
+'''
+
+#d=open("sample.txt","a")
+with open("sample.txt","w") as d:
+    d.write("bharat")
+    d.write(123)
     
-      
-              
-new.close()
-print("\n\t-----------Data Saved to file-----------------")
+    
